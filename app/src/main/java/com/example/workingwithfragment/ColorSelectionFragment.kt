4a0b26e7e3ba8fd910e6be35212843c7a2b5db7e
Jun Zhou <tun40202@temple.edu>
2023-10-17 12:10:37 -0400

    Initial commit

---
+++ app/src/main/java/com/example/workingwithfragment/ColorSelectionFragment.kt
@@ -0,0 +1,43 @@
+package com.example.workingwithfragment
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+
+class ColorSelectionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val layout = inflater.inflate(R.layout.fragment_color_selection, container, false)
+        val spinner = layout.findViewById<Spinner>(R.id.spinner)
+        spinner.adapter = ArrayAdapter(requireContext(),android.R.layout.simple_dropdown_item_1line, arrayOf("White", "Red", "Green","Blue","Yellow"))
+        spinner.onItemSelectedListener = object : OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                parent?.run {
+                    layout.setBackgroundColor(Color.parseColor(getItemAtPosition(position).toString()))
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+        return layout
+    }
+
+}
